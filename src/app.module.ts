d960c1285be0aa067f26a64f0cb51fcfc118a71b
i_swat@mail.ru <i_swat@mail.ru>
2022-03-21 01:18:39 +0300

    PostgreSQL. Sequelize. Подключаемся к базе данных.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { SequelizeModule} from "@nestjs/sequelize"
 
 @Module({
 	controllers: [AppController],
-	providers: [AppService]
+	providers: [AppService],
+	imports: [
+		SequelizeModule.forRoot({
+			dialect: "postgres",
+			host: "localhost",
+			port: 5432,
+			username: "postgres",
+			password: "root",
+			database: "nest-course",
+			models: [],
+			autoLoadModels: true
+		}),
+	],
 })
-export class AppModule{}
+export class AppModule {}
